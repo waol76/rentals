b0e65722338570f5f02a50283e698b7e152dfb29
Salvatore Berti <salvosan@Salvatores-iMac.local>
2025-01-07 13:41:53 +0100

    summarycards

--- src/components/RentalDashboard.tsx
+++ src/components/RentalDashboard.tsx
@@ -18,7 +18,7 @@ import {
   TooltipTrigger 
 } from '@/components/ui/tooltip';
 
-import GXXXrossNetIncomeWidget from '@/components/GrossNetIncomeWidget';
+import GrossNetIncomeWidget from '@/components/GrossNetIncomeWidget';
 import ExpenseBreakdownWidget from '@/components/ExpenseBreakdownWidget';
 import IncomeExpenseTable from '@/components/IncomeExpenseTable';
 import ApartmentComparisonWidgets from '@/components/ApartmentComparisonWidgets';
