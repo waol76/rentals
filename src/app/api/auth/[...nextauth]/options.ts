1adbf9798cca5ab64301a28611990e9caebae07e
Salvatore Berti <salvosan@Salvatores-iMac.local>
2025-03-09 12:30:57 +0100

    Fix authentication error page

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -5,6 +5,7 @@ import Google from 'next-auth/providers/google'
 const allowedEmails = [
   'waol76@gmail.com',
   'catua81@gmail.com',
+  'leonardoberti011@gmail.com',
   // Add more authorized emails as needed
 ];
 
